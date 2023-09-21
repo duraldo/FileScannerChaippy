36fc5e700362c1810ce53149269ec593f551cfad
Oscar <oscbissot@gmail.com>
2023-09-20 22:58:17 -0500

    Added a blacklist for system files, and added a input for scanning filetypes/folders (not working yet).

--- FileScannerChaippy01.py
+++ FileScannerChaippy01.py
@@ -2,8 +2,9 @@ import ctypes
 import sys
 import os
 
+blacklist = set(["pagefile.sys", "hiberfil.sys", "swapfile.sys"])
 
-# C:\Users\muska\source\repos\FileScannerChaippy\FileScannerChaippy01.py  (refference)
+# \repos\FileScannerChaippy\FileScannerChaippy01.py  (refference)
 
 def is_admin():
     """Check if the script is running with administrative privileges."""   #Be sure to run terminal in administrator mode for this to work.
@@ -26,9 +27,6 @@ def get_size(path="D:\\"):
         print(f"Permission defined for {path}. Skipping...")
     return total
 
-
-# this function gets the 10 largest files.
-
 def get_largest_files(path="D:\\", top_n=10):
     files = []
     try:
@@ -43,36 +41,50 @@ def get_largest_files(path="D:\\", top_n=10):
         print(f"Permission denied for {path}. Skipping...")
     return files[:top_n]
 
-
-#Main block
+#Changed to using list, as it will make adding to the code and maintaining it easier, later.
 if __name__ == "__main__":
     if not is_admin():
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-
-    # The scrip returns the 10 largest and filtiest files on the drive right now 
-      
+     
     else:
         user_input = input("Hey! Welcome to Chaippy File Scanner, would you like to scan for the top 10 biggest files? Yes? No?")
-        if user_input.lower() == "yes" or user_input.lower() == "y":   #Input yes to start scan
-            path_to_check = "D:\\"  # Entire Drive Scan
-            print(f"Total size of '{path_to_check}': {get_size(path_to_check) / (1024*1024):.2f} MB")
-            print("Top 10 largest files:")
-            for file, size in get_largest_files(path_to_check):
-                print(f"{file}: {size / (1024*1024):.2f} MB")
-        elif user_input.lower() == "no" or user_input.lower() == "n":   
+        if user_input.lower() in ["yes", "y"]:   
+            #Continues to ask for input. using while loop
+
+            #Top 10 files in the drive.
+            while True:
+                file_type = input("I'm not a mind reader, would you like to scan the whole drive or a specfic file or folder?")
+            
+                if file_type.lower() in ["drive", "d"]:
+                    path_to_check = "D:\\"  # Entire Drive Scan
+                    print(f"Total size of '{path_to_check}': {get_size(path_to_check) / (1024*1024):.2f} MB")
+                    print("Top 10 largest files:")
+                    for file, size in get_largest_files(path_to_check):
+                        print(f"{file}: {size / (1024*1024):.2f} MB")
+                    """Here, the user will be asked if there are any files from the list they want to remove"""
+                    ### Placeholder code for next time, we need to blacklist system files!! ###
+                    
+                    break
+                
+                elif file_type.lower() in ["folder", "f", "file","filetype"]:
+                    print ("Too bad, that feature doesn't work yet. I'm busy playing Starfield, I'll get to it eventually.")
+                    break
+
+                #break lets the scrip exit, so it doesn't keep asking.
+
+
+        elif user_input.lower() in ['no', 'n']: 
         # elif user_input.lower() == "no" or user_input.lower() == "n": can also be used.
+        # Lists are better for this.
 
             print("Okay, fine, be that way, bye! :P")
         else:
             print("Invalid Input. please enter correct input")
-            
-        input("Press Enter to continue...")   #add a pause, for the script, at end of main block.
 
 
-
-#Added the input/output.
 
 # Future notes: Look for other exceptions that might arise.
 # plan to add an input/output to seearch for smaller files, specific files, etc.
 # Eventually with options to suggest to remove files, etc. 
 # Maybe incorporate rasa open source to act as a chatbot?
+

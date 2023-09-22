928bba50ffe5e36e5f1daadf9271a516746696b6
Oscar <oscbissot@gmail.com>
2023-09-22 17:49:08 -0500

    Trying to clean up the branch code, File ScannerChaippy accidentyly created, trying to match the masterbranch.

--- FileScannerChaippy01.py
+++ FileScannerChaippy01.py
@@ -48,17 +48,27 @@ def get_largest_files(path="D:\\", top_n=10):
 if __name__ == "__main__":
     if not is_admin():
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-
-    # The scrip returns the 10 largest and filtiest files on the drive right now 
-    # Now adding user input/output to the ___main___ function  
+     
     else:
         user_input = input("Hey! Welcome to Chaippy File Scanner, would you like to scan for the top 10 biggest files? Yes? No?")
-        if user_input.lower() == "yes" or user_input.lower() == "y":   #Input yes to start scan
-            path_to_check = "D:\\"  # Entire Drive Scan
-            print(f"Total size of '{path_to_check}': {get_size(path_to_check) / (1024*1024):.2f} MB")
-            print("Top 10 largest files:")
-            for file, size in get_largest_files(path_to_check):
-                print(f"{file}: {size / (1024*1024):.2f} MB")
+        if user_input.lower() == "yes" or user_input.lower() == "y":   
+            #Continues to ask for input. using while loop
+
+            #Top 10 files in the drive.
+            while True:
+                file_type = input("I'm not a mind reader, would you like to scan the whole drive or a specfic folder?")
+            
+                if file_type.lower() == "drive":
+                    path_to_check = "D:\\"  # Entire Drive Scan
+                    print(f"Total size of '{path_to_check}': {get_size(path_to_check) / (1024*1024):.2f} MB")
+                    print("Top 10 largest files:")
+                    for file, size in get_largest_files(path_to_check):
+                        print(f"{file}: {size / (1024*1024):.2f} MB")
+                # if filetype is asked for, it doesn't work yet, placeholder.
+                elif file_type.lower() == "filetype" or file_type.lower() == "file":
+                    print ("Too bad, that feature doesn't work yet.")
+
+
         elif user_input.lower() == "no" or user_input.lower() == "n":   
         # elif user_input.lower() == "no" or user_input.lower() == "n": can also be used.
 
@@ -66,7 +76,7 @@ if __name__ == "__main__":
         else:
             print("Invalid Input. please enter correct input")
 
-#Added the input/output.
+
 
 # Future notes: Look for other exceptions that might arise.
 # plan to add an input/output to seearch for smaller files, specific files, etc.
